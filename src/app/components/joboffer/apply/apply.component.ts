09ab55ea3261a95dee13d5f42df80f3ab2d776b6
Vai van Ommeren <vaivanommeren+1@gmail.com>
2019-04-06 12:40:03 +0200

    added most of the support for applying for a job. Waiting on sessions to be active so I can take the user data from that. Currently miss the user password and id for the formvalidation to pass

--- src/app/components/joboffer/apply/apply.component.ts
+++ src/app/components/joboffer/apply/apply.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {JobofferService} from '../../../services/joboffer/joboffer.service';
+import {User} from '../../../domain/User';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'app-apply',
   templateUrl: './apply.component.html',
-  styleUrls: ['./apply.component.sass']
+  styleUrls: ['./apply.component.scss']
 })
 export class ApplyComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  id: string;
+
+  constructor(private route: ActivatedRoute,
+              private jobOfferService: JobofferService,
+              private snackbar: MatSnackBar,
+              private router: Router) {
+    this.id = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.jobOfferService.applyForJob(this.user, this.id).subscribe((jobofffer) => {
+      const snackbarRef = this.snackbar.open('Succesvol aangemeld voor de vacature', 'dismiss', {
+        duration: 3000
+      });
+      snackbarRef.afterDismissed().subscribe(() => {
+        this.router.navigateByUrl('/');
+      });
+    });
+  }
 }
--- src/app/services/joboffer/joboffer.service.ts
+++ src/app/services/joboffer/joboffer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {JobOffer} from '../../models/JobOffer';
 import {Observable} from 'rxjs';
+import {User} from '../../domain/User';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,9 @@ export class JobofferService {
 
   getJobOfferCount() {
     return this.httpClient.get<string>(this.jobOfferBaseUrl + '/all/count');
+  }
+
+  applyForJob(u: User, id: string) {
+    return this.httpClient.put(this.jobOfferBaseUrl + '/' + id, u);
   }
 }
