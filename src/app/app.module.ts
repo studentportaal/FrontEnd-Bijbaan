ee6baeba60dbccf6803034518339693e0b70079b
Kennyproz <k.odigwegomez@student.fontys.nl>
2019-04-08 12:35:20 +0200

    added usereditprofile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { JobofferlistComponent } from './components/joboffer/Offerslist/jobofferlist.component';
 import { LoginComponent } from './components/user/login/login.component';
+import { UsereditprofileComponent } from './components/user/usereditprofile/usereditprofile.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { LoginComponent } from './components/user/login/login.component';
     JobofferlistComponent,
     HomeComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    UsereditprofileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/user/usereditprofile/usereditprofile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usereditprofile',
+  templateUrl: './usereditprofile.component.html',
+  styleUrls: ['./usereditprofile.component.sass']
+})
+export class UsereditprofileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
