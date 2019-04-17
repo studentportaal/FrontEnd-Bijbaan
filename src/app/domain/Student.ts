b6037dcade8bf8287b5ad5cf02fd49e7871b7f06
Kennyproz <k.odigwegomez@student.fontys.nl>
2019-04-17 12:39:29 +0200

    bug fix

--- src/app/domain/Student.ts
+++ src/app/domain/Student.ts
@@ -1,12 +1,12 @@
-import { User } from "./User";
+import { User } from './User';
 
-export class Student extends User{
+export class Student extends User {
   firstName: string;
   lastName: string;
   dateOfBirth: Date;
   institute: string;
 
   constructor() {
-    super()
+    super();
   }
 }
