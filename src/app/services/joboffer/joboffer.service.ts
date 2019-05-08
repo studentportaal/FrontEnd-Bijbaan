8d271b3a197ef65f0672bb0b9c211def43b7bce2
MeijerM1 <max1_meijer@hotmail.com>
2019-05-08 09:36:29 +0200

    Replaced double quotes by single quotes

--- src/app/services/joboffer/joboffer.service.ts
+++ src/app/services/joboffer/joboffer.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {JobOffer} from '../../domain/JobOffer';
 import {Observable} from 'rxjs';
 import {Student} from '../../domain/Student';
-import {environment} from "../../../environments/environment";
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
