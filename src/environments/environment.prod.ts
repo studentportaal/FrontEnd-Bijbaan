a6f66e545a1abcdc004cc7ac2bffa35d8925d2f3
Vai van Ommeren <vaivanommeren+1@gmail.com>
2019-05-17 16:40:02 +0200

    added some vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE: 'http://api-dot-pts6-bijbaan.appspot.com'
+  API_BASE: 'http://api-dot-pts6-bijbaan.appspot.com',
+  FONTYS_AUTH: 'http://ineedaurl'
 };
