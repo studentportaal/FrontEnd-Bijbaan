2fae16e21d174bd28c9aed20802ecc00ecc024d8
Kennyproz <k.odigwegomez@student.fontys.nl>
2019-05-21 12:03:11 +0200

    bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,14 @@ import {MatSnackBar} from "@angular/material";
 })
 export class AppComponent implements OnInit {
   title = 'Jobby';
-  languageicon = 'gb';
+  languageicon = 'nl';
 
   constructor(public translate: TranslateService, public authenticationService: AuthenticationService, private route: ActivatedRoute, private snackbar: MatSnackBar, private router: Router) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+    translate.setDefaultLang('nl');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use('nl');
 
     this.route.queryParams.subscribe(params => {
       if (params['code'] != null) {
