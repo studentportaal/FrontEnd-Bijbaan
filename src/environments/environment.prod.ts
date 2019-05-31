a4f7a27e278182fafbb3f7cd3f67f01aaebce4aa
MeijerM1 <max1_meijer@hotmail.com>
2019-05-31 14:42:04 +0200

    Add other shit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   API_BASE: 'https://api-dot-pts6-bijbaan.appspot.com',
-  FONTYS_AUTH: 'https://fontys-auth-dot-pts6-bijbaan.appspot.com'
+  FONTYS_AUTH: 'https://fontys-auth-dot-pts6-bijbaan.appspot.com',
+  REVIEW_BASE: 'https://us-central1-pts6-bijbaan.cloudfunctions.net',
 };
