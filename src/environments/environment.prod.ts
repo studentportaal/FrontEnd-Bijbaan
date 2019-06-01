8a017fd83a1c85a018bf374fb68f8abf0c536567
Vai van Ommeren <vaivanommeren@gmail.com>
2019-06-01 17:47:37 +0200

    / too much

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   API_BASE: 'https://api-dot-pts6-bijbaan.appspot.com',
   FONTYS_AUTH: 'https://fontys-auth-dot-pts6-bijbaan.appspot.com',
   REVIEW_BASE: 'https://us-central1-pts6-bijbaan.cloudfunctions.net',
-  NOTIFICATION_BASE: 'https://europe-west1-pts6-bijbaan.cloudfunctions.net/'
+  NOTIFICATION_BASE: 'https://europe-west1-pts6-bijbaan.cloudfunctions.net'
 };
